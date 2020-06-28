fa49222ee158da2c4d43628c226f3019cbe25323
空城 <3136106883@qq.com>
2020-06-28 17:00:49 +0800

    随文档同步更新demo

--- demo.py
+++ demo.py
@@ -11,7 +11,6 @@ pi = pyiot.start("http://127.0.0.1:8888", "机器人的QQ号", pyiot.DEBUG)
 
 # 将这个函数注册为一个命令，当一条消息能够被装饰器传入的参数匹配时触发该函数（支持正则表达式）
 # 同一个函数可以使用多个装饰器来注册
-@pi.on_friend_command("(阿伟死了|awsl)")  # 当一条私聊消息以 阿伟死了 或 awsl 开头时触发
-@pi.on_group_command("awsl")  # 当一条群聊消息以 awsl 开头时触发
-def awsl(event):
-    pi.reply("阿伟：我没死！", event)  # 快捷回复，传入event，将消息发送给event所指向的群或好友
+@pi.on_friend_command("(你好|hello)")  # 当一条私聊消息能够被该正则表达式所匹配时触发
+def hello(event):
+    pi.reply("Hello World!", event)  # 快捷回复，传入event，将消息发送给event所指向的群或好友
